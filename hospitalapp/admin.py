ff03959a1fdaa291cca5ff9dcc61129ff56d786a
kasee18 <mutisyavincent99@gmail.com>
2025-08-05 16:28:02 +0300

    todays's update 05/08/2025

--- hospitalapp/admin.py
+++ hospitalapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from hospitalapp.models import * #import all *
+
 # Register your models here.
+admin.site.register(patient)
+admin.site.register(doctor)
+admin.site.register(ward)
+
--- hospitalapp/models.py
+++ hospitalapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class patient(models.Model):
+    firstname = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname + " " + self.surname
+
+class doctor(models.Model):
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField()
+    speciality = models.CharField(max_length=100)
+    maritalstatus = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    birthday = models.DateField()
+    appointmenttime = models.DateTimeField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.fullname
+
+class ward(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    department = models.CharField(max_length=100)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name + "  " + self.department + "  " + self.description
+
